15df335310d4db1dead5f067e8c4f00d23bb082d
Luffy <2020152146@ub.edu.bz>
2023-03-01 12:27:33 -0600

    added a serever and created a healthcheck handler to check that everthing works

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,12 +2,59 @@
 package main
 
 import (
-
+	"flag"
 	"fmt"
-	
+	"log"
+	"net/http"
+	"os"
+	"time"
 )
 
-fuunc main()
-{
-	fmt.Println{ "a journey of a thousand miles begins with a single step"}
+// global variable to hold the application
+// version number
+const version = "1.0.0"
+
+type config = struct {
+	port int
+	env  string// development, staging, production etc.
+}
+
+// setup dependency injection
+type application struct {
+	config config //variable name "config" of type config
+	logger *log.Logger
+}
+
+func main() {
+	var cfg config
+	//read in the flags that are needed to populate our config
+	//get the argumeents for the user for the server configuration
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment(development|staging|production)")
+	flag.Parse()
+	//create a logger
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	//Creat an instance of our application struct
+	//create an object if type application
+	app := &application{
+		config: cfg,
+		logger: logger, //for terminal
+
+	}
+	//create our new servemux
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+	//create our HTTP server
+
+	srv := &http.Server {
+		Addr: fmt.Sprintf(":%d", cfg.port),
+		Handler: mux,
+		IdleTimeout: time.Minute,
+		ReadTimeout: 10 * time.Second,
+		WriteTimeout:30 * time.Second,
+	}
+	//start our server
+	logger.Printf("Starting %s Server on port %d", cfg.env, srv.Addr)
+	err := srv.ListenAndServe()
+	logger.Fatal(err) //if error then print out the error
 }
