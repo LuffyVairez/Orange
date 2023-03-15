a616b44ede45e0e7a5c1b009c1ce35652de6b83b
Dalwin Lewis <lewisdalwin@gmail.com>
2023-03-15 15:21:23 -0600

    added JSON error handling

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -16,7 +16,7 @@ func (app *application) createSchoolHandler(w http.ResponseWriter, r *http.Reque
 func (app *application) showSchoolHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParams(r)
 	if err != nil {
-		http.NotFound(w, r)
+		app.notFoundResponse(w, r)
 		return
 	}
 	//fmt.Fprintf(w, "show details of school %d\n", id)
@@ -32,10 +32,9 @@ func (app *application) showSchoolHandler(w http.ResponseWriter, r *http.Request
 		Mode:     []string{"blended", "online", "face-to-face"},
 		Version:  1,
 	}
-	err = app.writeJSON(w, http.StatusOK, school, nil)
+	err = app.writeJSON(w, http.StatusOK, envelope{"school": school}, nil)
 	if err != nil {
-		app.logger.Println(err)
-		http.Error(w, "the server encountered a problem and could not process your request", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 		return
 	}
 }
